2c566e6c12b2a6dac5058ced46a59816fec6305e
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-21 00:17:08 +0000

    cleaned up codebase

--- config/middleware.js
+++ config/middleware.js
@@ -1,28 +1,28 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const isAuthenticated = (req, res, next) => {
-    const authHeader = req.get('Authorization');
+  const authHeader = req.get("Authorization");
 
-    if (!authHeader) {
-      return res.status(401).json({ error: 'Not authenticated.' });
-    }
+  if (!authHeader) {
+    return res.status(401).json({ error: "Not authenticated." });
+  }
 
-    const token = authHeader.split(' ')[1];
-    let decodedToken;
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
 
-    try {
-      decodedToken = jwt.verify(token, 'somesupersecretsecret');
-    } catch (err) {
-      return res.status(500).json({ error: err.message });
-    }
+  try {
+    decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 
-    if (!decodedToken) {
-        return res.status(401).json({ error: 'Not authenticated.' });
-    }
+  if (!decodedToken) {
+    return res.status(401).json({ error: "Not authenticated." });
+  }
 
-    req.userId = decodedToken.userId;
+  req.userId = decodedToken.userId;
 
-    next();
+  next();
 };
 
 module.exports = { isAuthenticated };
